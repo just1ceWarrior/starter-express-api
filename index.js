2d5b87dd59c732e4dce74493c6dc898168d4b23a
Saleh Alikhani <salehalikhani@gmail.com>
2024-02-17 19:35:18 +0330

    catch

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
 
-createVLESSServer(3000, "4da011a9-135c-4c13-a29d-0c4842f795ef");
+try {
+  createVLESSServer(3000, "4da011a9-135c-4c13-a29d-0c4842f795ef");
+} catch (err) {
+  console.log(err);
+}
